afb74a747437c2905ba637b5bdaf1f58de6bd2fe
Monique Baumann <monique@freedomcash.org>
2024-07-01 21:40:04 +0200

    free the world

--- usage-example-static.ts
+++ usage-example-static.ts
@@ -1,5 +1,4 @@
-import { FreedomSwaps } from "./mod.ts"
-// import { FreedomSwaps } from "https://deno.land/x/freedomswaps/mod.ts"
+import { FreedomSwaps } from "https://deno.land/x/freedomswaps/mod.ts"
 
 // Deno.args[0] // e.g. "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270" // Matic on Polygon POS
 // Deno.args[1] // e.g. "0x099471B71c9D8b0C6b616eE9A7C645e22cA9cfF7" // Freiheit on Polygon POS
